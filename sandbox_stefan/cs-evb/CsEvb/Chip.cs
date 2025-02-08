7dc322418edf827845e97aa6fc9fdaa7938cb1e2
Stefan Mauerhofer <stefan.mauerhofer@swisscom.com>
2025-02-08 17:52:14 +0100

    F18A & GA144 assenlber is compiling ROM code

---
+++ sandbox_stefan/cs-evb/CsEvb/Chip.cs
@@ -0,0 +1,26 @@
+﻿namespace CsEvb;
+
+public class Chip
+{
+  public virtual int NoOfRows()
+  {
+    throw new NotImplementedException();
+  }
+
+  public virtual int NoOfColumns()
+  {
+    throw new NotImplementedException();
+  }
+
+  public virtual int GetLastRow()
+  {
+    return NoOfRows() - 1;
+  }
+
+  public virtual int GetLastColumn()
+  {
+    return NoOfColumns() - 1;
+  }
+
+
+}
